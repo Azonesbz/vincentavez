392672ed96408495087d0f66bcf89b8873eb3c53
Vincent Avez <vincent.avez22@gmail.com>
2023-06-19 09:17:34 +0200

    added homepage

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Actuality from "../components/actuality/Actuality";
+import Header from "../components/header/Header";
+
+export default function Home(){
+    return (
+        <>
+            <Header />
+            <Actuality />
+        </>
+    )
+}
