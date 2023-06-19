70d4654d282b7e3dddc4331e42417ab588b86d17
Vincent Avez <vincent.avez22@gmail.com>
2023-06-19 15:28:38 +0200

    created portfolio component and added cards component

---
+++ client/src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,12 @@
+import Cards from "./cards";
+
+export default function Portfolio(){
+    return (
+        <>
+            <section className="container mt-10">
+                <h1 className="font-light">Les projets que j'ai réalisé</h1>
+                <Cards />
+            </section>
+        </>
+    )
+}
