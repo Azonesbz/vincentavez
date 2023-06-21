456757d4a068d44d3ce9ec12d25fbabfe8ab7914
Vincent Avez <vincent.avez22@gmail.com>
2023-06-21 19:28:10 +0200

    deleted light effect on cards component

--- client/src/components/portfolio/Cards.jsx
+++ client/src/components/portfolio/Cards.jsx
@@ -17,11 +17,8 @@ export default function Cards() {
                         alt="product image"
                     />
                 </Carousel>
-                <div className="flex flex-col w-full p-2 mt-3">
-                    <div className="absolute right-[15%] blur-sm flex -mb-px h-[1px] w-24">
-                        <div className="w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]" />
-                        <div className="-ml-[100%] w-full flex-none blur-[1px] [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]" />
-                    </div>
+                <div className="flex flex-col w-full p-2 mt-3 sm:mt-0 sm:pt-0">
+
                     <div className="flex justify-between items-start">
                         <h2 className="bg-clip-text text-transparent bg-gradient-to-r from-[#ff00aa] to-[#0000ff]">RoadMyTrip</h2>
                         <i className="text-sm">06/03/2023</i>
@@ -29,7 +26,7 @@ export default function Cards() {
                     <div className="flex flex-col space-y-2 mt-5">
                         <p>C'est toujours marrant de voir par quoi nous avons commencer. Voici mon premier site internet !</p>
                         <p>Grâce à ce site, j'ai appris à manipuler des données provenant d'une API</p>
-                        
+
                     </div>
                     <div className="flex flex-wrap gap-5 py-5">
                         <span className="inline-block bg-[#95bbe6] font-bold py-1 px-2 rounded-full">HTML</span>
@@ -63,7 +60,7 @@ export default function Cards() {
                         alt="product image"
                     />
                 </Carousel>
-                <div className="flex flex-col w-full p-2 mt-3">
+                <div className="flex flex-col w-full p-2 mt-3 sm:mt-0 sm:pt-0">
                     <div className="flex justify-between items-start">
                         <h2 className="bg-clip-text text-transparent bg-gradient-to-r from-[#ff00aa] to-[#0000ff]">SpaceZone</h2>
                         <i className="text-sm">06/03/2023</i>
