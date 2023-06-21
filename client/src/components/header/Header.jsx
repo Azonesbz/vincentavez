d3a04a4ed896fb06bd41a38c09f33052fda849d2
Vincent Avez <vincent.avez22@gmail.com>
2023-06-21 17:33:26 +0200

    added effect on navbar

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,27 +1,32 @@
 import './header.css';
 import React, { useContext } from 'react';
+import { useState } from 'react';
 import { useRef } from 'react';
 import { Link } from "react-router-dom";
 
-export default function Header(){
+export default function Header() {
     const actualityRef = useRef(null);
     const parcoursRef = useRef(null);
     const portfolioRef = useRef(null);
+
+    const [navbar, setNavbar] = useState(true);
+
     return (
         <header className='min-h-screen flex-col items-center justify-center'>
-            <nav className='flex items-center p-1 justify-between shadow relative inset-0 z-10'>
-                <img src="./VincentAvez.png" alt="logo du site" height={120} />
+            <nav className={`flex items-center p-1 justify-between shadow fixed top-0 z-10 w-full bg-white duration-200 ${navbar ? "visible": "opacity-0"}`} onMouseEnter={() => setNavbar(true)} onMouseMove={() => setNavbar(true)} onMouseLeave={() => setNavbar(false)}>
+                <Link to="." onClick={(e) => scrollTo(0, 0)}><img src="./VincentAvez.png" alt="logo du site" height={120} /></Link> 
                 <ul className='flex space-x-10 mr-10'>
                     <li className='item-link'><Link to={"#actuality"} onClick={() => actualityRef.current.scrollIntoView()}>Actualité</Link></li>
                     <li className='item-link'><Link to={"#parcours"} onClick={() => parcoursRef.current.scrollIntoView()}>Parcours</Link></li>
                     <li className='item-link'><Link to={"#portfolio"} onClick={() => portfolioRef.current.scrollIntoView()}>Portfolio</Link></li>
                 </ul>
+                <div className="absolute left-48 bottom-0 flex -mb-px h-[2px] w-56">
+                    <div className="w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]" />
+                    <div className="-ml-[100%] w-full flex-none blur-[1px] [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]" />
+                </div>
             </nav>
             <img className='absolute z-0 rotate-180 blur-2xl' src="/blurblue.png" alt="blur image" />
-            <div className="absolute left-48 flex -mb-px h-[2px] w-56">
-                <div className="w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]" />
-                <div className="-ml-[100%] w-full flex-none blur-[1px] [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]" />
-            </div>
+
             <div className='container items-center justify-center grid grid-cols-6 min-h-screen absolute inset-0'>
                 <h1 className='col-span-3'><span className='gradient-text'>Vincent </span>vous souhaite la bienvenue !</h1>
                 <div className='relative col-span-3 flex-col items-center justify-center p-10'>
@@ -29,7 +34,7 @@ export default function Header(){
                     <h2 className='text-center mt-5'>Développeur d'applications web et web mobile</h2>
                 </div>
             </div>
-            
+
         </header>
     )
 }
