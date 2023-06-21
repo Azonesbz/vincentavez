bd39d85a2cfcadace5bf2477aa0002686291a423
Vincent Avez <vincent.avez22@gmail.com>
2023-06-21 16:23:00 +0200

    updated cards

--- client/src/components/portfolio/Cards.jsx
+++ client/src/components/portfolio/Cards.jsx
@@ -17,8 +17,8 @@ export default function Cards() {
                         alt="product image"
                     />
                 </Carousel>
-                <div className="flex flex-col w-full pl-5 bg-neutral-100 shadow">
-                    <div className="absolute right-72 flex -mb-px h-[2px] w-56">
+                <div className="flex flex-col w-[40%] pl-5 bg-neutral-100 shadow">
+                    <div className="absolute right-[15%] blur-sm flex -mb-px h-[1px] w-24">
                         <div className="w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]" />
                         <div className="-ml-[100%] w-full flex-none blur-[1px] [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]" />
                     </div>
@@ -31,7 +31,7 @@ export default function Cards() {
                         <p>Grâce à ce site, j'ai appris à manipuler des données provenant d'une API</p>
                         
                     </div>
-                    <div className="flex space-x-5 py-5">
+                    <div className="flex flex-wrap gap-5 py-5">
                         <span className="inline-block bg-[#95bbe6] font-bold py-1 px-2 rounded-full">HTML</span>
                         <span className="inline-block bg-[#95bbe6] font-bold py-1 px-2 rounded-full">CSS</span>
                         <span className="inline-block bg-[#95bbe6] font-bold py-1 px-2 rounded-full">JavaScript</span>
@@ -40,30 +40,30 @@ export default function Cards() {
                     </div>
                 </div>
             </div>
-            <div className="mt-10 flex ">
+            <div className="mt-10 flex">
                 <Carousel>
                     <img
-                        className="min-w-full object-cover object-center"
+                        className="min-w-full object-contain object-center"
                         src="/spacezoneWelcome.png"
                         alt="product image"
                     />
                     <img
-                        className="min-w-full object-cover object-center"
+                        className="min-w-full object-contain object-center"
                         src="/spacezoneHeader.png"
                         alt="product image"
                     />
                     <img
-                        className="min-w-full object-cover object-center"
+                        className="min-w-full object-contain object-center"
                         src="/spacezoneProduct.png"
                         alt="product image"
                     />
                     <img
-                        className="min-w-full object-cover object-center"
+                        className="min-w-full object-contain object-center"
                         src="/spacezoneprofil.png"
                         alt="product image"
                     />
                 </Carousel>
-                <div className="flex flex-col w-full pl-5 bg-neutral-100 shadow">
+                <div className="flex flex-col w-[40%] pl-5 bg-neutral-100 shadow">
                     <div className="flex justify-between items-start p-2">
                         <h2 className="bg-clip-text text-transparent bg-gradient-to-r from-[#ff00aa] to-[#0000ff]">SpaceZone</h2>
                         <i>06/03/2023</i>
@@ -73,7 +73,7 @@ export default function Cards() {
                         <p>J'ai appris les fondamentaux de React, et j'ai aussi appris à utiliser Redux. C'était une vrai galère !</p>
                         <p>Système de panier, système de payement avec stripe, CRUD au complet côté administrateur et utilisateur, carousel fait maison, filtre de recherche, menu intéractif, réusabilité des composants, création de modal. Et encore plein de chose dont je suis très fier d'avoir réalisé.</p>
                     </div>
-                    <div className="flex space-x-5 py-5">
+                    <div className="flex flex-wrap gap-5 py-5">
                         <span className="inline-block bg-[#95bbe6] font-bold py-1 px-2 rounded-full">React.js</span>
                         <span className="inline-block bg-[#95bbe6] font-bold py-1 px-2 rounded-full">Redux</span>
                         <span className="inline-block bg-[#95bbe6] font-bold py-1 px-2 rounded-full">TailwindCSS</span>
