2e9e276ae4e39a0c84bb4387363140398f505379
Vincent Avez <vincent.avez22@gmail.com>
2023-06-24 19:52:59 +0200

    updated competence

--- client/src/components/career/Career.jsx
+++ client/src/components/career/Career.jsx
@@ -1,6 +1,36 @@
+import { useRef } from "react";
+import { useEffect } from "react";
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
-export default function Career(){
+export default function Career() {
+
+    const contentHtmlRef = useRef(null);
+    const contentCssRef = useRef(null);
+    const contentJsRef = useRef(null);
+    const contentReactRef = useRef(null);
+    const contentNodejsRef = useRef(null);
+
+    const [htmlCompetence, setHtmlCompetence] = useState(false)
+    const [cssCompetence, setCssCompetence] = useState(false)
+    const [jsCompetence, setJsCompetence] = useState(false)
+    const [reactCompetence, setReactCompetence] = useState(false)
+    const [nodejsCompetence, setNodejsCompetence] = useState(false)
+
+    useEffect(() => {
+        const contentHtmlElement = contentHtmlRef.current;
+        const contentCssElement = contentCssRef.current;
+        const contentJsElement = contentJsRef.current;
+        const contentReactElement = contentReactRef.current;
+        const contentNodejsElement = contentNodejsRef.current;
+
+        contentHtmlElement.style.height = htmlCompetence ? `${contentHtmlElement.scrollHeight}px` : '0px';
+        contentCssElement.style.height = cssCompetence ? `${contentCssElement.scrollHeight}px` : '0px';
+        contentJsElement.style.height = jsCompetence ? `${contentJsElement.scrollHeight}px` : '0px';
+        contentReactElement.style.height = reactCompetence ? `${contentReactElement.scrollHeight}px` : '0px';
+        contentNodejsElement.style.height = nodejsCompetence ? `${contentNodejsElement.scrollHeight}px` : '0px';
+    }, [htmlCompetence, cssCompetence, jsCompetence, reactCompetence, nodejsCompetence]);
+
     return (
         <>
             <section className="container min-h-screen mt-10 flex-grow w-full relative z-1 py-24" id="parcours">
@@ -29,10 +59,97 @@ export default function Career(){
                         <p className="mt-5">Je suis ouvert à toute proposition, veuillez me contacter sur mon adresse mail. <Link to="#contact" className="text-indigo-800">Ici</Link></p>
                     </div>
                 </div>
-                <div className="bg-stone-200 w-[90%] sm:w-[60%] mt-10 p-5 rounded-l-xl shadow ml-auto relative z-2">
+                <div className="bg-stone-200 w-[90%] sm:w-[60%] mt-10 p-5 rounded-l-xl shadow ml-auto relative z-2 flex flex-col space-y-5">
                     <h2>Compétences</h2>
-                    <div>
-                        
+                    <div className="relative" onClick={() => htmlCompetence ? setHtmlCompetence(false) : setHtmlCompetence(true)}>
+                        <div className='flex mr-5 space-x-2 items-center'>
+                            <svg className={`duration-500 ${htmlCompetence ? "" : "rotate-180"}`} width="25" height="25" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="m6 9 6 6 6-6"></path>
+                            </svg>
+                            <h3 className="cursor-pointer">HTML</h3>
+                        </div>
+                        <div className="border-l-[1px] border-stone-400 pl-2 ml-3 overflow-hidden">
+                            <div
+                                ref={contentHtmlRef}
+                                className="transition-height duration-500 ease-out"
+                            >
+                                <p>
+                                    Comme tout le monde, j'ai commencé par apprendre le langage HTML. Un langage de balises permettant de construire le squelette de votre application.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="relative" onClick={() => cssCompetence ? setCssCompetence(false) : setCssCompetence(true)}>
+                        <div className='flex mr-5 space-x-2 items-center'>
+                            <svg className={`duration-500 ${cssCompetence ? "" : "rotate-180"}`} width="25" height="25" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="m6 9 6 6 6-6"></path>
+                            </svg>
+                            <h3 className="cursor-pointer">CSS</h3>
+                        </div>
+                        <div className="border-l-[1px] border-stone-400 pl-2 ml-3 overflow-hidden">
+                            <div
+                                ref={contentCssRef}
+                                className="transition-height duration-500 ease-out"
+                            >
+                                <p>
+                                    Comme tout le monde, j'ai commencé par apprendre le langage HTML. Un langage de balises permettant de construire le squelette de votre application.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="relative" onClick={() => jsCompetence ? setJsCompetence(false) : setJsCompetence(true)}>
+                        <div className='flex mr-5 space-x-2 items-center'>
+                            <svg className={`duration-500 ${jsCompetence ? "" : "rotate-180"}`} width="25" height="25" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="m6 9 6 6 6-6"></path>
+                            </svg>
+                            <h3 className="cursor-pointer">JavaScript</h3>
+                        </div>
+                        <div className="border-l-[1px] border-stone-400 pl-2 ml-3 overflow-hidden">
+                            <div
+                                ref={contentJsRef}
+                                className="transition-height duration-500 ease-out"
+                            >
+                                <p>
+                                    Comme tout le monde, j'ai commencé par apprendre le langage HTML. Un langage de balises permettant de construire le squelette de votre application.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="relative" onClick={() => reactCompetence ? setReactCompetence(false) : setReactCompetence(true)}>
+                        <div className='flex mr-5 space-x-2 items-center'>
+                            <svg className={`duration-500 ${reactCompetence ? "" : "rotate-180"}`} width="25" height="25" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="m6 9 6 6 6-6"></path>
+                            </svg>
+                            <h3 className="cursor-pointer">React.js</h3>
+                        </div>
+                        <div className="border-l-[1px] border-stone-400 pl-2 ml-3 overflow-hidden">
+                            <div
+                                ref={contentReactRef}
+                                className="transition-height duration-500 ease-out"
+                            >
+                                <p>
+                                    Comme tout le monde, j'ai commencé par apprendre le langage HTML. Un langage de balises permettant de construire le squelette de votre application.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="relative" onClick={() => nodejsCompetence ? setNodejsCompetence(false) : setNodejsCompetence(true)}>
+                        <div className='flex mr-5 space-x-2 items-center'>
+                            <svg className={`duration-500 ${nodejsCompetence ? "" : "rotate-180"}`} width="25" height="25" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="m6 9 6 6 6-6"></path>
+                            </svg>
+                            <h3 className="cursor-pointer">NodeJS</h3>
+                        </div>
+                        <div className="border-l-[1px] border-stone-400 pl-2 ml-3 overflow-hidden">
+                            <div
+                                ref={contentNodejsRef}
+                                className="transition-height duration-500 ease-out"
+                            >
+                                <p>
+                                    Comme tout le monde, j'ai commencé par apprendre le langage HTML. Un langage de balises permettant de construire le squelette de votre application.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
