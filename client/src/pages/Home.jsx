ddd38f27e7460d093e6bfc052ab694d0a1a40216
Vincent Avez <vincent.avez22@gmail.com>
2023-06-25 18:05:34 +0200

    added lazy load for performance

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,14 +2,26 @@ import Actuality from "../components/actuality/Actuality";
 import Career from "../components/career/Career";
 import Header from "../components/header/header";
 import Portfolio from "../components/portfolio/portfolio";
+import LazyLoad from 'react-lazyload';
 
-export default function Home(){
+export default function Home() {
     return (
         <>
             <Header />
-            <Actuality />
-            <Career />
-            <Portfolio />
+
+            <LazyLoad height={200}>
+                <Actuality />
+            </LazyLoad>
+
+            <LazyLoad height={200}>
+                <Career />
+            </LazyLoad>
+
+            <LazyLoad height={200}>
+                <Portfolio />
+            </LazyLoad>
+
+
         </>
     )
 }
