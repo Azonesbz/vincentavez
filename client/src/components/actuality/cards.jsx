3c6c30642ddc4b11b7f8e093d60f0287eac5e831
Vincent Avez <vincent.avez22@gmail.com>
2023-06-27 15:56:50 +0200

    added style and replaced carousel

--- client/src/components/actuality/cards.jsx
+++ client/src/components/actuality/cards.jsx
@@ -1,24 +1,43 @@
-import Carousel from "../carousel/Carousel";
+import LazyLoad from "react-lazyload";
+import useEmblaCarousel from 'embla-carousel-react'
 import { Link } from "react-router-dom";
+import "./Actuality.css"
+
+const embla_OPTIONS = {
+    loop: true,
+    align: 'center',
+}
 
 export default function Cards() {
+    const [vivaRef] = useEmblaCarousel(embla_OPTIONS)
     return (
         <>
-            <div className="mt-10 flex flex-col sm:flex-row bg-neutral-100 shadow-xl">
-                <Carousel>
-                    <img
-                        className="min-w-full object-cover object-center"
-                        src="/viva2.webp"
-                        alt="product image"
-                    />
-                    <img
-                        className="min-w-full object-cover object-center"
-                        src="/viva1.webp"
-                        alt="product image"
-                    />
-                </Carousel>
-                <div className="flex flex-col w-full p-5 mt-3">
-                    
+            <div className="mt-10 flex flex-col col-span-12 md:col-span-4">
+                <div className="embla" ref={vivaRef}>
+                    <div className="embla__container">
+                        <div className="embla__slide">
+                            <LazyLoad className="min-h-full" once>
+                                <img
+                                    className="min-h-full h-72 object-cover object-center"
+                                    src="/viva2.webp"
+                                    alt="product image"
+                                />
+                            </LazyLoad>
+                        </div>
+                        <div className="embla__slide">
+                            <LazyLoad className="min-h-full" once>
+                                <img
+                                    className="min-h-full h-72 object-cover object-center"
+                                    src="/viva1.webp"
+                                    alt="product image"
+                                />
+                            </LazyLoad>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex flex-col p-5 shadow-xl">
+
                     <div className="flex justify-between items-start">
                         <h2>Viva Technology</h2>
                         <i className="text-sm">17/06/2023</i>
@@ -29,7 +48,7 @@ export default function Cards() {
                         <p>J'ai réaliser une interview et j'ai également présenter mon parcours ce qui m'a permis de renforcer ma confiance à l'oral, parce que c'était pas gagné !</p>
                         <p>Je vous invites grandement si l'occasion se présente à vous, de participer à un évènement de ce style.</p>
                     </div>
-                    
+
                 </div>
             </div>
         </>
