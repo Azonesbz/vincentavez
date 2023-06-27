300decebe0bd21b38323dd27d820b74fc9d86ff9
Vincent Avez <vincent.avez22@gmail.com>
2023-06-27 09:07:55 +0200

    adjusted section width and added grid. Added cooperation composant

--- client/src/components/portfolio/Portfolio.jsx
+++ client/src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,21 @@
-import Cards from "./cards";
+import Project from "./Projects";
+import "./Portfolio.css"
+import Cooperation from "./CooperationProject";
+
+
 
 export default function Portfolio(){
     return (
         <>
-            <section className="container py-24" id="portfolio">
+            <section className="container py-24 " id="portfolio">
                 <h1 className="font-light">Les projets que j'ai réalisé</h1>
-                <Cards />
+                <div className="grid grid-cols-12 gap-5">
+                    <Project />
+                </div>
+                <h1 className="text-center mt-10">En collaboration</h1>
+                <div className="grid grid-cols-12 gap-5">
+                    <Cooperation />
+                </div>
             </section>
         </>
     )
