8940e17b68b370752011e1867c6ee884d050bd53
Vincent Avez <vincent.avez22@gmail.com>
2023-06-27 16:55:33 +0200

    added diploma content to actuality component

--- client/src/components/actuality/cards.jsx
+++ client/src/components/actuality/cards.jsx
@@ -16,7 +16,7 @@ export default function Cards() {
                 <div className="embla" ref={vivaRef}>
                     <div className="embla__container">
                         <div className="embla__slide">
-                            <LazyLoad className="min-h-full" once>
+                            <LazyLoad className="min-h-full" once height={250}>
                                 <img
                                     className="min-h-full h-72 object-cover object-center"
                                     src="/viva2.webp"
@@ -25,7 +25,7 @@ export default function Cards() {
                             </LazyLoad>
                         </div>
                         <div className="embla__slide">
-                            <LazyLoad className="min-h-full" once>
+                            <LazyLoad className="min-h-full" once height={250}>
                                 <img
                                     className="min-h-full h-72 object-cover object-center"
                                     src="/viva1.webp"
@@ -47,6 +47,34 @@ export default function Cards() {
                         <p>En effet, pôle emploi m'a invité pour que je puisse représenter la <Link className="text-indigo-800" to="https://3wa.fr">3WAcademy</Link>.</p>
                         <p>J'ai réaliser une interview et j'ai également présenter mon parcours ce qui m'a permis de renforcer ma confiance à l'oral, parce que c'était pas gagné !</p>
                         <p>Je vous invites grandement si l'occasion se présente à vous, de participer à un évènement de ce style.</p>
+                    </div>
+
+                </div>
+            </div>
+            <div className="mt-10 flex flex-col col-span-12 md:col-span-4">
+                <div className="embla" ref={vivaRef}>
+                    <div className="embla__container">
+                        <div className="embla__slide">
+                            <LazyLoad className="min-h-full" once height={250}>
+                                <img
+                                    className="min-h-full h-72 object-cover object-center"
+                                    src="/diplome.webp"
+                                    alt="product image"
+                                />
+                            </LazyLoad>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex flex-col p-5 shadow-xl">
+
+                    <div className="flex justify-between items-start">
+                        <h2>J'ai obtenu mon diplôme !</h2>
+                        <i className="text-sm">22/05/2023</i>
+                    </div>
+                    <div className="flex flex-col space-y-2 mt-5">
+                        <p>Ce vendredi 12 mai, j'ai passé une présentation orale pour mon projet <a className="text-indigo-800" href="#spacezone">SpaceZone</a>. J'ai réussi à le réaliser en seulement 1 mois, après avoir abandonné <a className="text-indigo-800" href="#roadmytrip">RoadMyTrip</a>, et cela s'est avéré être le meilleur choix que j'aurais pu faire !</p>
+                        <p>Nous étions une promotion de 12 apprenants, avec un formateur qui nous a enseigné les bases du métier de développeur web. Après 5 mois de travail acharné, j'ai finalement réussi !</p>
                     </div>
 
                 </div>
