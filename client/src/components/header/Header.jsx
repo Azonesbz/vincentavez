32b409943cec143653e89e33715c4bf6ddeee3de
Vincent Avez <vincent.avez22@gmail.com>
2023-06-27 16:03:04 +0200

    fixed of a few bugs

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,17 +1,17 @@
-import './header.css';
+import './Header.css';
 import React from 'react';
 
 export default function Header() {
-
     return (
         <header className='min-h-screen max-h-screen w-full  flex flex-col items-center justify-center overflow-x-hidden'>
-            <div className='h-[60px] w-full bg-white fixed top-0 z-40 opacity-75 backdrop-blur-sm'/>
+            <div className='h-[60px] w-full bg-white fixed top-0 z-40 opacity-75 backdrop-blur-sm' />
             <nav className={`container shadow fixed top-0 max-w-full z-50 flex items-center w-full h-[60px] backdrop-blur-md`}>
-                <a className='ml-5' href='.'><img src="./logo.webp" alt="logo du site" width={50} /></a>
+                <a className='ml-5' href='#'><img src="./logo.webp" alt="logo du site" width={50} /></a>
                 <ul className='space-x-10 mr-10 hidden sm:flex items-center ml-10 h-full '>
                     <li className='item-link'><a href={"#actuality"}>Actualité</a></li>
                     <li className='item-link'><a href={"#parcours"}>Parcours</a></li>
                     <li className='item-link'><a href={"#portfolio"}>Portfolio</a></li>
+                    <li className='item-link'><a href={"#contact"}>Contact</a></li>
                 </ul>
                 <div className="absolute left-[20%] bottom-0 flex -mb-px h-[2px] w-56">
                     <div className="w-full flex-none [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]" />
@@ -28,13 +28,14 @@ export default function Header() {
                         <a href="#actuality">Actualité</a>
                         <a href="#parcours">Parcours</a>
                         <a href="#portfolio">Portfolio</a>
+                        <a href="#contact">Contact</a>
                     </div>
                 </div>
             </nav>
 
             <h1 className='text-center'>Vincent vous souhaite la bienvenue !</h1>
             <div className='relative flex flex-col-reverse items-center justify-center '>
-                <img className='image mt-10' src="/me.webp" alt="picture of Vincent" width={200}/>
+                <img className='image mt-10' src="/me.webp" alt="picture of Vincent" width={200} />
                 <h1 className='font-thin text-2xl text-center gradient-text'>Développeur d'applications web et web mobile</h1>
             </div>
             <a
