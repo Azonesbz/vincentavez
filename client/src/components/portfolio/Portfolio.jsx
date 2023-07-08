de06512e42ffd5a0716cdd96ea0714a69e6fefcb
Azones <59911700+Azonesbz@users.noreply.github.com>
2023-07-08 14:02:06 +0200

    fixed font weight and tag

--- client/src/components/portfolio/Portfolio.jsx
+++ client/src/components/portfolio/Portfolio.jsx
@@ -8,11 +8,11 @@ export default function Portfolio(){
     return (
         <>
             <section className="container py-24 " id="portfolio">
-                <h1 className="font-light">Les projets que j'ai réalisé</h1>
+                <h2>Les projets que j'ai réalisé</h2>
                 <div className="grid grid-cols-12 gap-5">
                     <Project />
                 </div>
-                <h1 className="text-center mt-10">En collaboration</h1>
+                <h2 className="text-center mt-10">En collaboration</h2>
                 <div className="grid grid-cols-12 gap-5">
                     <Cooperation />
                 </div>
