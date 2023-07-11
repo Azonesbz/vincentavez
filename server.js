2fac1867ada634655cab43aeca3215b3514e94bf
Azones <59911700+Azonesbz@users.noreply.github.com>
2023-07-11 12:05:56 +0200

    added secure protocol to server

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ import express from 'express'
 import dotenv from 'dotenv'
 import router from './src/router.js'
 import cors from 'cors'
+import https from "https"
+import fs from "fs"
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+const options = {
+    key: fs.readFileSync('https_cert/privkey.pem'),
+    cert: fs.readFileSync('https_cert/cert.pem'),
+  };
+
 app.use(express.static('public'));
+app.use(express.json({ limit: "100mb" }));
 app.use(cors())
 app.use(router)
 
-app.listen(PORT, () => {
+https.createServer(options, app).listen(PORT, () => {
     console.log(`Le serveur écoute sur le port ${PORT}`)
 })
--- src/config/db.js
+++ src/config/db.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-console.log(process.env.USER_DB)
 const options =
 {
     host: process.env.HOST_DB,
     user: process.env.USER_DB,
     database: process.env.DATABASE_DB,
-    password: process.env.password
+    password: process.env.password,
+    port: 3306,
 }
 export const connection = mysql.createConnection(options).promise();
